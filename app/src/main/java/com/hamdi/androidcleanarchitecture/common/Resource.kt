0921e8f295100ec8a7f3c4d2927ee4983631ffeb
ItachiUchiha0 <dev.ahmedhamdi@gmail.com>
2021-10-27 23:10:21 +0200

    Adding notes

--- app/src/main/java/com/hamdi/androidcleanarchitecture/common/Resource.kt
+++ app/src/main/java/com/hamdi/androidcleanarchitecture/common/Resource.kt
@@ -1,7 +1,7 @@
 package com.hamdi.androidcleanarchitecture.common
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T) : Resource<T>(data) //add the type in use cases
+    class Success<T>(data: T) : Resource<T>(data) // add the type in use cases
     class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
     class Loading<T>(data: T? = null) : Resource<T>(data)
 }
